f3104af65e746af910d71c5dc0a509aa007f79ce
ElijahOffutt <Einzenheim@yahoo.com>
2024-03-14 21:57:09 -0400

    updates updates updates

---
+++ portfolio/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <left />
+    <right />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import left from './components/navs/left'
+import right from './components/navs/right'
+//
+</script>
---
+++ portfolio/src/components/navs/right.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-navigation-drawer app location="right">
+        <v-list-item title="Connect" />
+        <v-list-item>
+            <v-row dense>
+                <v-col cols="6">
+                    <v-btn block size="small" color="#008A00">sign up</v-btn>
+                </v-col>
+                <v-col cols="6">
+                    <v-btn block size="small" color="#0050EF">login</v-btn>
+                </v-col>
+            </v-row>
+        </v-list-item>
+        <v-divider class="my-2" />
+        <v-list-item title="Tools & Projects" subtitle="My recent obsessions">
+            <br />
+            <v-btn block size="large">
+                Edge AI
+            </v-btn>
+        </v-list-item>
+    </v-navigation-drawer>
+</template>
